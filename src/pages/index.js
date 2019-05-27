defe6277607c65118cf0cded2ecaf73da74ba4fd
Ryan Hill <ryanhill@Ryans-MBP.home>
2019-05-27 14:57:08 -0400

    Grass attempt/youtube

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ const IndexPage = () => (
       <div className="HeroGroup">
         <h1 />
         <p>GARAGEBAND PUNK</p>
-        <a href="https://www.instagram.com/please_band/">Watch the video NOW</a>
+        <a href="https://youtu.be/KDkE0HG7zTw">Watch the video NOW</a>
         <div className="Logos">
           <img src={require('../images/please-hero.png')} width="100" />
           <img src={require('../images/flint-hero.jpg')} width="100" />
